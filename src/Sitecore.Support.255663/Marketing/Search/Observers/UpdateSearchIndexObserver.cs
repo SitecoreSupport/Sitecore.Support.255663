2c246897e21ecbecb803e6648561f766d1ddaf1f
Paul Stupka <paul.stupka@sitecore.com>
2020-05-05 19:34:35 +0300

    base implementation

---
+++ src/Sitecore.Support.255663/Marketing/Search/Observers/UpdateSearchIndexObserver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sitecore.ContentSearch;
+using Sitecore.Diagnostics;
+using Sitecore.Marketing.Definitions;
+using Sitecore.StringExtensions;
+
+namespace Sitecore.Support.Marketing.Search.Observers
+{
+  public class UpdateSearchIndexObserver<T> : Sitecore.Marketing.Search.Observers.UpdateSearchIndexObserver<T> where T : IDefinition
+    {
+        private readonly ISearchIndex searchIndex;
+
+        public UpdateSearchIndexObserver([NotNull] string databaseName, [NotNull] string searchIndex) : base(databaseName, searchIndex)
+        {
+            Assert.ArgumentNotNull(searchIndex, "searchIndex");
+            this.searchIndex = ContentSearchManager.GetIndex(searchIndex);
+        }
+
+        public override void ProcessNotification([NotNull] IReadOnlyCollection<T> definitions)
+        {
+            Assert.ArgumentNotNull(definitions, "definitions");
+            var indexables = new List<SitecoreItemUniqueId>();
+            foreach (var id in definitions.Select(x => x.Id))
+            {
+                var item = base.Database.GetItem(id);
+                Assert.IsNotNull(item, "Unable to update marketing definitions search index. The definition id is not found: '{0}'.", id);
+
+                foreach (var itemUri in item.Versions.GetVersions(true).Select(vi => vi.Uri))
+                {
+                    Log.Info("Updating marketing definitions search index: '{0}'.".FormatWith(id), this);
+                    indexables.Add(new SitecoreItemUniqueId(itemUri));
+                }
+            }
+
+            this.searchIndex.Update(indexables);
+        }
+    }
+}
--- src/Sitecore.Support.255663/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.255663/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.255663")]
 [assembly: AssemblyProduct("Sitecore.Support.255663")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("UpdateSearchIndexObserver might throw an exception during application restart")]
+
